612d547d33815473102c6543705930ad3a29baa2
Spiewak <bspiewak6@gmail.com>
2020-11-05 16:48:16 -0500

    updated post routes and tested successfully

--- models/index.js
+++ models/index.js
@@ -1,4 +1,16 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+  
+
 module.exports = { User, Post };
