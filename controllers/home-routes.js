2d603a506a551a0e1897b7f4651482181c2ca472
Spiewak <bspiewak6@gmail.com>
2020-11-06 13:17:47 -0500

    updated homepage and main handlbars - started to style

---
+++ controllers/home-routes.js
@@ -0,0 +1,43 @@
+// this file will contain all of the user-facing routes
+// homepage and login page
+
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        // pass a single post object into the homepage template
+        // loop over and map each Sequelize object saving the results in a new posts array
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
