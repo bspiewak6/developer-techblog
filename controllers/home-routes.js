2832884c190144e7954a54afb6a6e6e669897f88
Spiewak <bspiewak6@gmail.com>
2020-11-06 15:23:33 -0500

    added signup functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,6 +5,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+// route to render the homepage
 router.get('/', (req, res) => {
     Post.findAll({
       attributes: [
@@ -38,6 +39,11 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
-  });
+});
+
+// route to render the login and signup page
+router.get('/login', (req, res) => {
+  res.render('login');
+});
 
 module.exports = router;
